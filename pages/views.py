db50b0b88b8525634f3d0bb2c51979ab71342fd2
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-04-07 04:16:58 +0000

    created a new url /contact

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args, **kwargs):
     return HttpResponse("<h1> Hello World </h1>") #string of HTML Code
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1> This is the contact page!</h1>")
